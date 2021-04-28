8f3bec2739c34536b5c48786e1d1ea56db6faa83
unknown <wiemgargouri2000@gmail.com>
2021-04-28 18:20:07 +0100

    delete and create are ready

--- server/services/render.js
+++ server/services/render.js
@@ -1,4 +1,31 @@
 exports.homeRoutes = (req,res)=>{
-    res.render('index');
+     //Make a get request to api/users
+     axios.get("http://localhost:3000/api/users")
+     .then(function(response){
+       
+        res.render('index',{users:response.data});
+        
+     })
+     .catch(err=>{
+         res.send(err)
+     })
+   
+}
+
+
+exports.adduser = (req,res)=>{
+   
+    res.render('adduser');
+}
+
+exports.UpdateUser = (req,res)=>{
+    axios.get("http://localhost:3000/api/users",{params:{id:req.query.id}})
+    .then(function(userdata){
+        res.render('update',{user:userdata.data})
+    })
+    .catch(err=>{
+        res.send(err)
+    })
+  
 }
 
